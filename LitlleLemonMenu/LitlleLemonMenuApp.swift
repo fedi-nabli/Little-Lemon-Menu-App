bffa75b73917445615859127a4814de718298aef
Fedi Nabli <fedinabli@gmail.com>
2023-01-11 13:16:22 +0100

    Initial Commit

---
+++ LitlleLemonMenu/LitlleLemonMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitlleLemonMenuApp.swift
+//  LitlleLemonMenu
+//
+//  Created by Fedi Nabli on 11/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct LitlleLemonMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
