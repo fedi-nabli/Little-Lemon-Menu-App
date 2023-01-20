de0a5a521d4c76060f6b06e86132685a49841dc0
Fedi Nabli <fedinabli@gmail.com>
2023-01-20 14:24:58 +0100

    Added filter option view

--- LitlleLemonMenu/MenuItemsOptionView.swift
+++ LitlleLemonMenu/MenuItemsOptionView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct MenuItemsOptionView: View {
+    let selectedCategories = [Categories.Food,Categories.Drink, Categories.Desserts]
+    
+    let sortBy = [SortBy.popular,SortBy.price, SortBy.range]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            
+            VStack (alignment: .leading, spacing: 10) {
+                HStack {
+                    Spacer()
+                    NavigationLink (destination: MenuItemsView()
+                        .navigationBarBackButtonHidden()){
+                            Text("Done")
+                                .font(.title2)
+                        }
+                }
+                Text("Filter")
+                    .font(.largeTitle)
+                    .fontDesign(.monospaced)
+                    .bold()
+                Section (header: Text("Selected Categories")
+                    .font(.system(size:  24))
+                    .foregroundColor(.gray)){
+                        List {
+                            ForEach (selectedCategories, id:  \.self){element in
+                                Text(element.rawValue)
+                            }
+                        }
+                        .padding([.leading,.trailing,.top ],-10)
+                    }
+                    
+                Section (header: Text("Sort By")
+                    .font(.system(size: 24))
+                    .foregroundColor(.gray)){
+                        List {
+                            ForEach (sortBy, id:  \.self){element in
+                                Text(element.rawValue)
+                            }
+                        }.padding([.leading,.trailing,.top],-10)
+                }
+                Spacer()
+            }
+            
+            .padding(10)
+            .background(Color.gray.opacity(0.1))
+            
+        }
+        
     }
 }
 
--- LitlleLemonMenu/MenuItemsView.swift
+++ LitlleLemonMenu/MenuItemsView.swift
@@ -8,95 +8,92 @@
 import SwiftUI
 
 struct MenuItemsView: View {
-    let numberCol = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    let numberCol = [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
     
-    let ingredients = [Ingredient.pasta, Ingredient.spaghetti, Ingredient.tomatoSauce, Ingredient.carrot, Ingredient.broccoli]
+    let ingredients = [Ingredient.broccoli,Ingredient.pasta,Ingredient.spinach,Ingredient.tomatoSauce,Ingredient.carrot]
     
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading, spacing: 20) {
-                HStack {
-                    Spacer()
-                    NavigationLink(destination: MenuItemDetailsView().navigationBarBackButtonHidden()) {
-                        Image(systemName: "line.3.horizontal.decrease.circle")
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                    }
-                }
-                
-                VStack(alignment: .leading) {
-                    Text("Menu")
-                        .font(.largeTitle)
-                        .fontDesign(.monospaced)
-                        .bold()
-                        .frame(alignment: .leading)
-                }
-                
-                ScrollView {
-                    Grid (alignment: .leading) {
-                        Text("Food")
-                            .font(.title2)
-                        
-                        LazyVGrid(columns: numberCol) {
-                            ForEach (1...12, id: \.self) {
-                                element in
-                                NavigationLink(destination: MenuItemDetailsView()) {
-                                    MenuFormat(id: element, item: "Food")
-                                }
-                            }
-                        }
-                        
-                        Text("Drink")
-                            .font(.title2)
-                        
-                        LazyVGrid(columns: numberCol) {
-                            ForEach (1...8, id: \.self) {
-                                element in
-                                NavigationLink(destination: MenuItemDetailsView()) {
-                                    MenuFormat(id: element, item: "Drink")
-                                }
-                            }
-                        }
-                        
-                        Text("Dessert")
-                            .font(.title2)
-                        
-                        LazyVGrid(columns: numberCol) {
-                            ForEach (1...4, id: \.self) {
-                                element in
-                                NavigationLink(destination: MenuItemDetailsView()) {
-                                    MenuFormat(id: element, item: "Food")
-                                }
-                            }
+                VStack( alignment: .leading, spacing: 20) {
+                    HStack {
+                        Spacer()
+                        NavigationLink (destination: MenuItemsOptionView()
+                            .navigationBarBackButtonHidden()){
+                            Image(systemName: "calendar")
+                                
                         }
                     }
+                    
+                    VStack (alignment: .leading) {
+                        Text("Menu")
+                            .font(.largeTitle)
+                            .fontDesign(.monospaced)
+                            .bold()
+                            .frame(alignment: .leading)
+                    }
+                    
+                    ScrollView {
+                        Grid (alignment: .leading) {
+                            
+                            
+                            Text("Food")
+                                .font(.title2)
+                            LazyVGrid(columns: numberCol) {
+                                ForEach(1...12, id: \.self ){element in
+                                    NavigationLink (destination: MenuItemDetailsView()) {
+                                        MenuFormat(id: element, item: "Food")
+                                    }
+                                }
+                            }
+                          
+                            Text("Drinks")
+                                .font(.title2)
+                            LazyVGrid(columns: numberCol) {
+                                ForEach(1...8, id: \.self ){element in
+                                    NavigationLink (destination: MenuItemDetailsView()) {
+                                        MenuFormat(id: element, item: "Drink")
+                                    }
+                                }
+                            }
+                            
+                            Text("Dessert")
+                                .font(.title2)
+                            LazyVGrid(columns: numberCol) {
+                                ForEach(1...4, id: \.self ){element in
+                                    NavigationLink (destination: MenuItemDetailsView()) {
+                                        MenuFormat(id: element, item: "Dessert")
+                                    }
+                                }
+                            }
+                            
+                        }
+                       
+                    }
+                    .scrollContentBackground(.hidden)
                 }
-            }
-            .scrollContentBackground(.hidden)
+                .padding(20)
         }
-        .padding(20)
     }
-}
-
-struct MenuItemsView_Previews: PreviewProvider {
-    static var previews: some View {
-        MenuItemsView()
-    }
-}
-
-struct MenuFormat: View {
-    let id: Int
-    let item: String
     
-    var body: some View {
-        VStack {
-            Rectangle()
-                .fill(.black.opacity(0.85))
-                .frame(height: 80)
-                .cornerRadius(6)
-            
-            Text("\(item) \(id)")
-                .foregroundColor(.black)
+    struct MenuItemsView_Previews: PreviewProvider {
+        static var previews: some View {
+            MenuItemsView()
+        }
+    }
+    
+    struct MenuFormat: View {
+        let id: Int
+        let item: String
+        var body: some View {
+            VStack{
+                Rectangle()
+                    .fill(.black)
+                    .frame(height: 80)
+                    .cornerRadius(6)
+                
+                Text("\(item) \(id)")
+                    .foregroundColor(.black)
+            }
         }
     }
 }
