8269d46f23b6a5797acf2184897879a5d41eaa69
Fedi Nabli <fedinabli@gmail.com>
2023-01-20 09:28:12 +0100

    Added Ingredient enum

---
+++ LitlleLemonMenu/Enums/Ingedient.swift
@@ -0,0 +1,16 @@
+//
+//  Ingedient.swift
+//  LitlleLemonMenu
+//
+//  Created by Fedi Nabli on 20/1/2023.
+//
+
+enum Ingredient: String {
+    case spinach = "Spinach"
+    case spaghetti = "Spaghetti"
+    case pasta = "Pasta"
+    case tomato = "Tomato"
+    case broccoli = "Brocolli"
+    case carrot = "Carrot"
+    case tomatoSauce = "Tomato Sauce"
+}
